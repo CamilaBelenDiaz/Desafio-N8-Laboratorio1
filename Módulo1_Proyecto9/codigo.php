54a62fffa7f35408a00fe62779c83920d60a9dcd
CamilaBelenDiaz <camilabdiaz09@gmail.com>
2026-07-04 20:40:36 -0300

    Add files via upload

---
+++ Módulo1_Proyecto9/codigo.php
@@ -0,0 +1,9 @@
+<?php
+
+// Tomamos los valores enviados por el formulario
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+// Mostramos el mensaje
+echo "Hola tu nombre es $usuario y tu contraseña es $password";
+?>
